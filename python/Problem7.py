f62222627cca8bd36396206febc9480a18a87641
Atul <kkmonlee@gmail.com>
2017-01-05 00:38:48 +0000

    Added problem 7

---
+++ python/Problem7.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 0:
+            return -self.reverse(-x)
+
+        result = 0
+        while x:
+            result = result * 10 + x % 10
+            x /= 10
+        return result if result <= 0x7fffffff else 0  # Handle overflow.
+
+    
+if __name__ == "__main__":
+    print Solution().reverse(123)
+    print Solution().reverse(-321)
